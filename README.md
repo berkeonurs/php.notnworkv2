c98d456cb4404c1a5687f8692972db1d0611c713
Berke Onur Süneçli <onursunecli@hotmail.com>
2019-08-11 16:43:05 +0300

    addNotes.php Created and Readme.md Updated for addNotes WebService

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 # notnwork v2.0
 notnwork WebService
-
-# Create User
+# USER
+## Create User
  
 | Route | HTTP Verb | POST body | Result | Description |
 | --- | --- | --- | --- | --- |
-| /ws/createUser.php | `POST` | {'key':'1453', 'which':'student, 'userName':'Berke Onur', 'userLastName':'Süneçli', 'userMail':berke@ogr.edu.tr, 'userPass':"123", 'userPhone': 055555555, 'studentClass': 2, 'departmentId':1 } | {'result':1 => User Created Successfully, 'result':305 => Account Already Exists, 'result':300 => Not a student mail} | Create a new Student User. |
+| createUser.php | `POST` | {'key':'1453', 'which':'student, 'userName':'Berke Onur', 'userLastName':'Süneçli', 'userMail':berke@ogr.edu.tr, 'userPass':"123", 'userPhone': 055555555, 'studentClass': 2, 'departmentId':1 } | {'result':1 => User Created Successfully, 'result':305 => Account Already Exists, 'result':300 => Not a student mail} | Create a new Student User. |
 
-# Login User
+## Login User
  
 | Route | HTTP Verb | POST body | Result | Description |
 | --- | --- | --- | --- | --- |
-| /ws/loginUser.php | `POST` | {'key':'1453', 'userMail':berke@ogr.edu.tr, 'userPass':"123"} | {'result':1 => Active User, 'result':400 => Banned User, 'result':404 => Unknown User} | Login User. |
+| loginUser.php | `POST` | {'key':'1453', 'userMail':berke@ogr.edu.tr, 'userPass':"123"} | {'result':1 => Active User, 'result':400 => Banned User, 'result':404 => Unknown User} | Login User. |
+
+# NOTES
+## Add Notes
+
+| Route | HTTP Verb | POST body | Result | Description |
+| --- | --- | --- | --- | --- |
+| addNotes.php | `POST` | {'key':'1453', 'userToken':5ebe96fc8d7e896f3b15adbe2941b0fb, 'noteTitle':"Fizik 1 Vize Notu", 'noteLesson':"Fizik 1", 'noteDesc':"Fizik 1 Son hafta vize notları", 'noteType':"Vize", 'noteTeacherName':"Kaan Manisa", 'departmentID': 1, "noteActive":1 or 2, $_FILES['img']} | {'result':1 => Notes Added Succesfully, 'result':26 => Not Image, 'result':33 => No Images Selected, 'result':0  => Key or Token Failed} | Add Notes. |
  
---
+++ addNotes.php
@@ -0,0 +1,81 @@
+<?php
+require_once 'config.php';
+require_once 'lib/upload.php';
+
+$key= filter_input(INPUT_POST, 'key');
+$userToken = filter_input(INPUT_POST, 'userToken');
+$results = ['result'=>0];
+
+if ($key == '1453' &&  $_SERVER['REQUEST_METHOD'] == 'POST' && isset($userToken)){
+    //DB Bağlantısı
+    $db = getDbInstance();
+
+    $data = array_filter($_POST);
+
+    //User Tablosu Sorgu
+    $db->where('userToken',$userToken);
+    $userSelected = $db->getOne('users');
+
+    if ($userSelected['userToken'] == $userToken){
+        unset($data['key']);
+        unset($data['userToken']);
+        $data['userId'] = $userSelected['id'];
+        $id = $db->insert('notes',$data);
+        if (isset($id)){
+
+            $images = array();
+            foreach ($_FILES['img'] as $k => $l) {
+                foreach ($l as $i => $v) {
+                    if (!array_key_exists($i, $images))
+                        $images[$i] = array();
+                    $images[$i][$k] = $v;
+                }
+            }
+
+
+            $dataImg['noteId'] = $id;
+            foreach ($images as $image){
+
+                $handle = new Upload($image);
+                if ($handle->uploaded) {
+                    $rand = md5(uniqid(mt_rand(), true));
+                    /* Resmi Yeniden Adlandır */
+                    $handle->file_new_name_body = $rand;
+
+
+                    /* Resim Yükleme İzni */
+                    $handle->allowed = array('image/*');
+
+                    /* Resmi İşle */
+                    $handle->Process("upload/");
+                    if ($handle->processed) {
+                        $urlImg['imgResult'] = $rand.'.'.$handle->image_src_type;
+                        $dataImg['imageUrl'] = returnUrl().$urlImg['imgResult'];
+                        $db->insert('notesimages',$dataImg);
+                        $results['result'] = 1;
+                    } else {
+                        $results['img'] = 26;
+                        $db->where('id',$id);
+                        $db->delete('notes');
+                        $results['result'] = 26; // Gelen resim değil
+                    }
+
+                    $handle-> Clean();
+
+                } else {
+                    $results['img'] = 33;
+                    $db->where('id',$id);
+                    $db->delete('notes');
+                    $results['result'] = 33; // Resim Seçilemedi
+                }
+
+            }
+
+
+        }
+
+
+    }
+
+}
+echo json_encode($results);
