afbf21163b33dd0bf439e8c997f688cf1adc3c85
Berke Onur Süneçli <onursunecli@hotmail.com>
2019-09-24 22:49:42 +0300

    archive notes updated

--- getArchiveNotes.php
+++ getArchiveNotes.php
@@ -12,12 +12,21 @@ if ($key == '1453' &&  $_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $data = array_filter($_POST);
 
-    $db->join('users u','n.userId=u.id','INNER');
-    $db->join('notesimages i','n.noteId=i.notesId','INNER');
-    $db->Where('universityId',$universityId);
-    $notesList = $db->get('notes n',null,'n.noteId,n.noteTitle,n.noteLesson,n.noteDesc,n.noteType,n.noteDate,n.noteTeacherListId,n.noteTeacherName,n.departmentId,n.noteActive,n.universityId,i.id,i.imageUrl,i.notesId,u.userName,u.userLastName,u.userPhoto');
-    $note = makeArray($notesList,'noteId',['id','imageUrl','noteId']);
-    $results = $note;
+    if (isset($data['universityId'])) {
+
+        $db->join('users u', 'n.userId=u.id', 'INNER');
+        $db->join('notesimages i', 'n.noteId=i.notesId', 'INNER');
+        $db->Where('universityId', $universityId);
+        $notesList = $db->get('notes n', null, 'n.noteId,n.noteTitle,n.noteLesson,n.noteDesc,n.noteType,n.noteDate,n.noteTeacherListId,n.noteTeacherName,n.departmentId,n.noteActive,n.universityId,i.id,i.imageUrl,i.notesId,u.userName,u.userLastName,u.userPhoto');
+        $note = makeArray($notesList, 'noteId', ['id', 'imageUrl', 'noteId']);
+        $results = $note;
+    }else{
+        $db->join('users u', 'n.userId=u.id', 'INNER');
+        $db->join('notesimages i', 'n.noteId=i.notesId', 'INNER');
+        $notesList = $db->get('notes n', null, 'n.noteId,n.noteTitle,n.noteLesson,n.noteDesc,n.noteType,n.noteDate,n.noteTeacherListId,n.noteTeacherName,n.departmentId,n.noteActive,n.universityId,i.id,i.imageUrl,i.notesId,u.userName,u.userLastName,u.userPhoto');
+        $note = makeArray($notesList, 'noteId', ['id', 'imageUrl', 'noteId']);
+        $results = $note;
+    }
 }
 
 echo json_encode($results);
